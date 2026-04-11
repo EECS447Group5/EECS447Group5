8012be5ecf39b6e3b75c914c40e26693d12e4b69
willbhedges <willbhedges@icloud.com>
2026-04-11 13:01:54 -0500

    clean up deprecation

--- stock-market-simulator/app/login/page.tsx
+++ stock-market-simulator/app/login/page.tsx
@@ -9,14 +9,15 @@ export default function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    function handleSubmit(e: React.FormEvent) {
-        e.preventDefault();
-        console.log(username, password);
-    }
 
     return (
         <div>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            console.log(username, password)
+        }
+
+        }>
             <label htmlFor="username">Username: </label><br />
             <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)}></input><br />
 
