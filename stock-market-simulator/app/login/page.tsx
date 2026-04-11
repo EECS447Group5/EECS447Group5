1359394e0acabcc0ffd264948ce9ed4fae4cbac6
willbhedges <willbhedges@icloud.com>
2026-04-11 12:57:47 -0500

    add basic login page

---
+++ stock-market-simulator/app/login/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+
+import { useState } from 'react'
+
+
+
+export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+        console.log(username, password);
+    }
+
+    return (
+        <div>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username: </label><br />
+            <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)}></input><br />
+
+            <label htmlFor="password">Password: </label><br />
+            <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}></input><br />
+            <button type="submit">Log In</button>
+        </form>
+
+        
+        </div>
+    )
+}
