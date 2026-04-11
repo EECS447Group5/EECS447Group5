28133cc3295af54f78077de96057ebb29ead7c77
willbhedges <willbhedges@icloud.com>
2026-04-11 12:14:49 -0500

    add a simple stock list

--- stock-market-simulator/app/trade/page.tsx
+++ stock-market-simulator/app/trade/page.tsx
@@ -1,3 +1,29 @@
+import Link from "next/link"
+
+const stocks = [
+    { ticker: "AAPL", name: "Apple Inc." },
+    { ticker: "MSFT", name: "Microsoft Corporation" },
+    {ticker: "GOOGL", name: "Alphabet Inc." },
+    {ticker: "AMZN", name: "Amazon.com Inc." },
+    { ticker: "NVDA", name: "NVIDIA Corporation" },
+    { ticker: "TSLA", name: "Tesla Inc." },
+    { ticker: "META", name: "Meta Platforms Inc." },
+];
+
 export default function TradePage() {
-    return <h1>Trade</h1>;
+    return (
+    
+        <div>
+            <h1>Trade</h1>
+            <ul>
+                {stocks.map((stock) => (
+                    <li key={stock.ticker}>
+                        <Link href={`/trade/${stock.ticker}`}>
+                            {stock.ticker} - {stock.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
