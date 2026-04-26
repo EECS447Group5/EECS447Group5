cdadec54c18e26977298e11f41cbaa6119deb24d
Eric Loseke <143624230+eloseke@users.noreply.github.com>
2026-04-26 18:38:12 -0500

    Use serverless instead of standard TCP for pool

--- stock-market-simulator/lib/db.ts
+++ stock-market-simulator/lib/db.ts
@@ -1,5 +1,9 @@
-import { neon, NeonQueryFunction } from '@neondatabase/serverless';
-import { Pool } from 'pg';
+import { neon, NeonQueryFunction, Pool, neonConfig } from '@neondatabase/serverless';
+import ws from 'ws';
+
+if (process.env.NODE_ENV !== 'production') {
+  neonConfig.webSocketConstructor = ws;
+}
 
 const connectionString = process.env.DB_URL;
 
@@ -16,7 +20,5 @@ export const pool = globalForPool.pool || new Pool({
       rejectUnauthorized: false
   }
 });
-
-if (process.env.NODE_ENV !== 'production') globalForPool.pool = pool;
 
 export const sql: NeonQueryFunction<false, false> = neon(connectionString);
