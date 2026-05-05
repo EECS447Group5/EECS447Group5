6ceca782dbd1186320e8dddf8495e1b96e5d8983
willbhedges <113948816+willbhedges@users.noreply.github.com>
2026-05-05 12:19:26 -0500

    limit pools of postgres

--- stock-market-simulator/lib/db.ts
+++ stock-market-simulator/lib/db.ts
@@ -15,10 +15,16 @@ const globalForPool = global as unknown as { pool: Pool };
 
 export const pool = globalForPool.pool || new Pool({
   connectionString: process.env.DB_URL,
-  max: 10,
+  max: 1,
+  idleTimeoutMillis: 10_000,
+  connectionTimeoutMillis: 5_000,
   ssl: {
       rejectUnauthorized: false
   }
 });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPool.pool = pool;
+}
 
 export const sql: NeonQueryFunction<false, false> = neon(connectionString);
