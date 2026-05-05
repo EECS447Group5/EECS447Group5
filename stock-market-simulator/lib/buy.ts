f849d722db36de4b8f722b333ed2a75c585e8eb8
willbhedges <113948816+willbhedges@users.noreply.github.com>
2026-05-05 12:22:08 -0500

    fix buy sell transactions

--- stock-market-simulator/lib/buy.ts
+++ stock-market-simulator/lib/buy.ts
@@ -3,30 +3,33 @@
 import { pool } from './db';
 
 export async function buyStock(user_id: number, ticker: string, quantity: number, price_at_trade: number) {
+    const client = await pool.connect();
     try {
-        await pool.query('BEGIN');
-        const res = await pool.query(`
+        await client.query('BEGIN');
+        const res = await client.query(`
             INSERT INTO transactions (user_id, stock_id, type, quantity, price_at_trade, trade_date)
             VALUES ($1, (SELECT stock_id FROM stocks WHERE ticker = $2), 'BUY', $3, $4, CURRENT_TIMESTAMP)
             RETURNING transaction_id;
         `, [user_id, ticker, quantity, price_at_trade]);
-        await pool.query(`
+        await client.query(`
             INSERT INTO holdings (user_id, stock_id, quantity)
             VALUES ($1, (SELECT stock_id FROM stocks WHERE ticker = $2), $3)
             ON CONFLICT (user_id, stock_id)
             DO UPDATE SET
                 quantity = holdings.quantity + EXCLUDED.quantity
                 `, [user_id, ticker, quantity]);
-        await pool.query(`
+        await client.query(`
             UPDATE users
             SET balance = balance - $1
             WHERE user_id = $2;
         `, [quantity * price_at_trade, user_id]);
-        await pool.query('COMMIT');
+        await client.query('COMMIT');
         return res.rows[0].transaction_id;
     } catch (err) {
-        await pool.query('ROLLBACK');
+        await client.query('ROLLBACK');
         console.error("Error buying stock: ", err);
         throw err;
+    } finally {
+        client.release();
     }
 }
--- stock-market-simulator/lib/sell.ts
+++ stock-market-simulator/lib/sell.ts
@@ -3,39 +3,42 @@
 import { pool } from './db';
 
 export async function sellStock(user_id: number, ticker: string, quantity: number, price_at_trade: number) {
+    const client = await pool.connect();
     try {
-        await pool.query('BEGIN');
-        await pool.query(`
+        await client.query('BEGIN');
+        await client.query(`
             INSERT INTO transactions (user_id, stock_id, type, quantity, price_at_trade, trade_date)
             VALUES ($1, (SELECT stock_id FROM stocks WHERE ticker = $2), 'SELL', $3, $4, CURRENT_TIMESTAMP);
             `, [user_id, ticker, quantity, price_at_trade]);
-        const res = await pool.query(`
+        const res = await client.query(`
             SELECT quantity
             FROM holdings
             WHERE user_id = $1 AND stock_id = (SELECT stock_id FROM stocks WHERE ticker = $2);
             `, [user_id, ticker]);
         if (res.rows[0].quantity === quantity) {
-            await pool.query(`
+            await client.query(`
                 DELETE FROM holdings
                 WHERE user_id = $1 AND stock_id = (SELECT stock_id FROM stocks WHERE ticker = $2);
                 `, [user_id, ticker]);
         }
         else {
-            await pool.query(`
+            await client.query(`
                 UPDATE holdings
                 SET quantity = quantity - $1
                 WHERE user_id = $2 AND stock_id = (SELECT stock_id FROM stocks WHERE ticker = $3);
                 `, [quantity, user_id, ticker]);
         }
-        await pool.query(`
+        await client.query(`
             UPDATE users
             SET balance = balance + $1
             WHERE user_id = $2;
         `, [quantity * price_at_trade, user_id]);
-        await pool.query('COMMIT');
+        await client.query('COMMIT');
     } catch (err) {
-        await pool.query('ROLLBACK');
+        await client.query('ROLLBACK');
         console.error("Error selling stock: ", err);
         throw err;
+    } finally {
+        client.release();
     }
 }
